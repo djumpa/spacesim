b19081dc4b0d80e604fdc7cccb0fe9e43a13e90e
Filip <FildaRak@seznam.cz>
2019-10-09 22:31:16 +0200

    Skeleton code for WebGL copied from Mozilla tut

---
+++ webroot/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+    <title>spacesim</title>
+
+  </head>
+
+  <body>
+  <div id="canvas-holder">
+    <canvas id="glcanvas"></canvas>
+</div>
+  </body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.8.1/gl-matrix-min.js"></script>
+<script src="satellite.js"></script>
+<script src="webgl-spacesim.js"></script>
+  
+</html>
